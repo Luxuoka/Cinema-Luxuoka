3c1dfc705a1ae8a4b3d9ae55f978628d34cde762
Luxuoka <cikalidcamp@gmail.com>
2026-05-10 20:33:40 +0700

    update sitemap routes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     sitemap({
-      hostname: 'https://cinema-luxuoka.vercel.app'
+      hostname: 'https://cinema-luxuoka.vercel.app',
+      dynamicRoutes: [
+        '/',
+        '/trending',
+        '/movies',
+        '/series',
+        '/watchlist',
+        '/profile',
+        '/genre',
+        '/watch',
+        '/anime',
+      ]
     })
   ],
   server: {
